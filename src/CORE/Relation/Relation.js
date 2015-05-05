2e7ca65ba9fbc612c9a17ef813009523fa304c7d
Oguz Gelal <o.gelal77@gmail.com>
2015-05-05 15:22:43 +0300

    relation class added

---
+++ src/CORE/Relation/Relation.js
@@ -0,0 +1,31 @@
+/**
+@class CORE.Relation
+@constructor
+@param {String} name Name of the relation
+*/
+
+define(function(){
+	
+	function Relation(name) {
+		/**
+		For identifying an instance
+		@property type
+		@type String
+		*/
+		this.type = "relation";
+		/**
+		Name of the entity
+		@property name
+		@type String
+		*/
+		this.name = name;
+		/**
+		The ontology this entity is created under
+		@property ontology
+		@type Object
+		*/
+		this.ontology = null;
+	}
+
+	return Relation;
+});
